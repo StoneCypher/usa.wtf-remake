5d570d74aabf2056e32a0fc676db27e3e8e401b2
John Haugeland <stonecypher@gmail.com>
2021-01-10 03:09:18 -0800

    starting to look nice

--- src/newsite.js
+++ src/newsite.js
@@ -9,8 +9,8 @@ const data = require('../build/data.js');
 
 
 
-const sen_header = '    <h1>These are the eight senators who participated in the coup</h1>\n\n',
-      rep_header = '    <h1>These are the 139 members of the House of Representatives who participated in the coup</h1><p>Listed in alphabetical order</p>\n\n';  // todo fix explanation
+const sen_header = '    <h1><a name="senators"></a>These are the eight senators who participated in the coup</h1>\n\n',
+      rep_header = '    <h1><a name="representatives"></a>These are the 139 members of the House of Representatives who participated in the coup</h1><p>Listed in alphabetical order</p>\n\n';  // todo fix explanation
 
 
 
@@ -20,7 +20,16 @@ data.senators.forEach(       sen => the_states[sen.state] = true);
 
 
 
-const fname_state = (state) =>
+const state_header = state =>
+  `<h1>${
+    state === 'all'
+      ? 'All states'
+      : `State page for ${state}`
+  }</h1>`;
+
+
+
+const fname_state = state =>
   `index_${
     state.toLowerCase()
          .replace(' ', '_')
@@ -28,32 +37,42 @@ const fname_state = (state) =>
 
 
 
-const header = `
+const header = for_state => {
+
+  const subrow = `
+        <ul class="subrow">
+          <li><a href="#senators">Senators</a></li>
+          <li><a href="#representatives">Representatives</a></li>
+        </ul>
+  `;
+
+  const maybe_all_current_class  =       for_state === 'all'? ' current' : '',
+        maybe_all_subrow         =       for_state === 'all'? subrow     : '',
+        maybe_each_current_class = sn => for_state === sn   ? ' current' : '',
+        maybe_each_subrow        = sn => for_state === sn   ? subrow     : '';
+
+  return `
       <div id="statelist">
-        <a href="index.html">All states</a> -
+        <a href="index.html" class="all${maybe_all_current_class}">All states</a>
+        ${ maybe_all_subrow }
         ${
         Object.keys(the_states)
               .sort()
-              .map(sn => `        <a href="${fname_state(sn)}">${sn}</a>\n`)
+              .map(sn =>
+                `        <a class="${sn.replace(' ', '_')}${
+                  maybe_each_current_class(sn)
+                }" href="${
+                  fname_state(sn)
+                }">${
+                  sn
+                }</a>${
+                  maybe_each_subrow(sn)
+                }\n`)
               .join(' ')
         }
       </div>
-      <div id="main">
+      <div id="main"><div id="mainbox">
 `;
-
-
-
-function newsite() {
-
-  return preface
-       + header
-       + prologue
-       + sen_header
-       + '<pre>' + JSON.stringify(data.senators, undefined, 2) + '</pre>'
-       + rep_header
-       + '<pre>' + JSON.stringify(data.representatives, undefined, 2) + '</pre>'
-       + postface;
-
 }
 
 
@@ -61,8 +80,8 @@ function newsite() {
 function billboard_senator(this_senator) {
   return `
 <div>
-  <h1>${this_senator.senator} of ${this_senator.state}</h1>
-  <h2>Offices:</h2>
+  <h2>${this_senator.senator} of ${this_senator.state}</h2>
+  <h3>Offices:</h3>
   <ol>
     ${this_senator.office.map(single_office => `
     <li>
@@ -96,13 +115,14 @@ function summarize_senator(this_senator) {
 function billboard_representative(this_representative) {
   return `
 <div>
-  <h1>${this_representative.representative} of ${this_representative.state}</h1>
-  <h2>Offices:</h2>
+  <h2>${this_representative.representative} of ${this_representative.state}</h2>
+  <h3>Offices:</h3>
   <ol>
     ${this_representative.office.map(single_office => `
     <li>
       <div>
-        ${single_office.addr.map( (a, i) => `<div class="addr addr_${i}">${a}</div>`).join('')}
+        ${single_office.addr.map( (a, i) =>
+          `<div class="addr addr_${i}">${a}</div>`).join('') }
       </div>${single_office.phone
         ? single_office.phone.map(p => `<div class="phone">Phone: ${p}</div>`).join('')
         : ''
@@ -124,12 +144,33 @@ function summarize_representative(this_representative) {
 
 
 
+function newsite() {
+
+  return preface
+       + header('all')
+       + state_header('all')
+       + prologue
+       + sen_header
+       + data.senators
+             .map(this_sen => billboard_senator(this_sen))
+             .join('')
+       + rep_header
+       + data.representatives
+             .map(this_rep => billboard_representative(this_rep))
+             .join('')
+       + postface;
+
+}
+
+
+
 function newsite_state(which_state) {
 
   let prev_state = null;
 
   return preface
-       + header
+       + header(which_state)
+       + state_header(which_state)
        + prologue
        + sen_header
        + '<div class="this_state">'

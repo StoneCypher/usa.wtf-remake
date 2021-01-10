a67d332ece3a905d2635f2132cb245c50c736516
John Haugeland <stonecypher@gmail.com>
2021-01-09 22:09:17 -0800

    trash tier parsing

--- src/host_parsers.js
+++ src/host_parsers.js
@@ -1,5 +1,9 @@
 
+const fs   = require('fs'),
+      reps = `${fs.readFileSync('./src/representatives.html')}`,
+      sens = `${fs.readFileSync('./src/senators.html')}`;
+
 const peg_reps = require('../build/peg_representatives.js'),
       peg_sens = require('../build/peg_senators.js');
 
-console.log('yep');
+console.log( peg_sens.parse(sens) );

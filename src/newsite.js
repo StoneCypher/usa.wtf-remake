72a135848a8fcd796a59972df7c173203f1c791c
John Haugeland <stonecypher@gmail.com>
2021-01-10 01:28:30 -0800

    basic billboarding and summarizing senators.  set up css, unused though

--- src/newsite.js
+++ src/newsite.js
@@ -29,7 +29,7 @@ const fname_state = (state) =>
 
 
 const header = `
-      <div id="header">
+      <div id="statelist">
         <a href="index.html">All states</a> -
         ${
         Object.keys(the_states)
@@ -58,14 +58,53 @@ function newsite() {
 
 
 
+function billboard_senator(this_senator) {
+  return `
+<div>
+  <h1>${this_senator.senator} of ${this_senator.state}</h1>
+  <h2>Offices:</h2>
+  <ol>
+    ${this_senator.office.map(single_office => `
+    <li>
+      <div>
+        ${single_office.addr.map(a => `<div>${a}</div>`).join('')}
+      </div>${single_office.phone
+        ? single_office.phone.map(p => `<div>Phone: ${p}</div>`).join('')
+        : ''
+      }${single_office.fax
+        ? single_office.fax.map(f => `<div>Fax: ${f}</div>`).join('')
+        : ''
+      }
+    </li>`
+    ).join('')}
+  </ol>
+</div>`.trim();
+}
+
+
+
+function summarize_senator(this_senator) {
+  return `<div>${this_senator.senator} of ${this_senator.state}</div>`
+}
+
+
+
 function newsite_state(which_state) {
 
   return preface
        + header
        + prologue
        + sen_header
-       + '<pre>' + JSON.stringify(data.senators.filter(s => s.state === which_state), undefined, 2) + '</pre>'
-       + '<pre>' + JSON.stringify(data.senators.filter(s => s.state !== which_state), undefined, 2) + '</pre>'
+       + data.senators
+             .filter(s => s.state === which_state)
+             .map(this_sen => billboard_senator(this_sen))
+             .join('')
+       + '<div class="other_state">'
+       + data.senators
+             .filter(s => s.state !== which_state)
+             .map(this_sen => summarize_senator(this_sen))
+             .join('')
+       + '</div>'
        + rep_header
        + '<pre>' + JSON.stringify(data.representatives.filter(r => r.state === which_state), undefined, 2) + '</pre>'
        + '<pre>' + JSON.stringify(data.representatives.filter(r => r.state !== which_state), undefined, 2) + '</pre>'
